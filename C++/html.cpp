f0ce2b3e09e21b3dae3aac220bec5c2a285f6383
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-05-22 20:16:46 +0500

    practice

---
+++ C++/html.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Content-Type: text/html\n\n";
+    std::cout << "<html><body>";
+    std::cout << "<h1>Hello, World!</h1>";
+    std::cout << "</body></html>";
+    return 0;
+}
